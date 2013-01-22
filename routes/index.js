370f7448f8cf659d478e2703e699dd3e71e51c88
Adam Wootton <superadamwo@hotmail.com>
2013-01-22 17:52:35 -0500

    changed id

--- routes/index.js
+++ routes/index.js
@@ -7,21 +7,25 @@ var message = new gcm.Message();
 message.addData('key1', 'vicisdick');
 var sender = new gcm.Sender('AIzaSyAS69XB59u8lPtxrpYWfuDjkSzvGz43xP8');
 var registrationIds = [];
+var startHitting = function() {
+	
+}
 
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
 
 exports.register = function(req, res) {
-	var registrationIds = [req.body['regId']];
+	registrationIds.push[req.body['regId']];
 	sender.send(message, registrationIds, 4, function(err, result) {
 		console.log(result);
 	})
 	res.send('sup');
+	startHitting();
 };
-
 setInterval(function() {
-	registrationIds = ["924240467687"];
+	registrationIds = ["APA91bF0MpHrnVXs7LbCUgDoYiSDjzzgZmIY5xTCPRHC8hObHskQ2tKtrC1opdRmt8N9JH5FaxPJg-FAn58MVXy-xWLKycVCvnoQDbW5QC-iVDSr08i9I9qXkJwU-kPcin1uyPs8F8phUuRXyjb-poYQdGMO4Uc8tIOlfQZfrIMnT9K6F3FGqko
+"];
 	sender.send(message, registrationIds, 4, function(err, result) {
 		console.log(result);
 	})
