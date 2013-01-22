48cfe6a47204d5781720da8d5c59bee7d90d2bdf
Adam Wootton <superadamwo@hotmail.com>
2013-01-22 17:43:48 -0500

    hitting victor

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.configure(function(){
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
-
+var device = mongoose.Schema({device_id: "string"});
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
@@ -47,7 +47,6 @@ app.post('/register', routes.register);
 request("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22RIMM%22)%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json", function(err, response, body){
   console.log(body);
 })
-
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,27 @@
 /*
  * GET home page.
  */
- var gcm = require('node-gcm');
+var gcm = require('node-gcm');
+var message = new gcm.Message();
+message.addData('key1', 'vicisdick');
+var sender = new gcm.Sender('AIzaSyAS69XB59u8lPtxrpYWfuDjkSzvGz43xP8');
+var registrationIds = [];
 
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
 exports.register = function(req, res) {
-	var message = new gcm.Message();
-	message.addData('key1', 'vicisdick');
-	var sender = new gcm.Sender('AIzaSyAS69XB59u8lPtxrpYWfuDjkSzvGz43xP8');
-	console.log(req.body);
 	var registrationIds = [req.body['regId']];
 	sender.send(message, registrationIds, 4, function(err, result) {
 		console.log(result);
 	})
 	res.send('sup');
-}
+};
+
+setInterval(function() {
+	registrationIds.push(924240467687);
+	sender.send(message, registrationIds, 4, function(err, result) {
+		console.log(result);
+	})
+}, 200);
