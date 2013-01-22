b7c6c19144ed4152adb57eb2484043e08c7f7cf1
Adam Wootton <superadamwo@hotmail.com>
2013-01-22 17:33:36 -0500

    logging things

--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,12 @@ exports.index = function(req, res){
 };
 exports.register = function(req, res) {
 	var message = new gcm.Message();
+	message.addData('key1', 'vicisdick');
 	var sender = new gcm.Sender('AIzaSyAS69XB59u8lPtxrpYWfuDjkSzvGz43xP8');
+	console.log(req.body);
 	var registrationIds = [req.body['regId']];
-	sender.send(message, registrationIds,4,function(err, result) {
+	sender.send(message, registrationIds, 4, function(err, result) {
 		console.log(result);
 	})
-	res.send('sup')
+	res.send('sup');
 }
